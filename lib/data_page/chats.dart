1813349047de33017b4aa9fab71167469a632c4d
Sohid Ullah <mdsohidullahchowdhury1@gmail.com>
2024-03-28 16:04:28 +0600

    Wroking

--- lib/data_page/chats.dart
+++ lib/data_page/chats.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, avoid_print
-
 import 'package:flutter/material.dart';
 //import 'package:flutter/widgets.dart';
 
@@ -11,6 +10,8 @@ class Chats extends StatefulWidget {
 }
 
 class _ChatsState extends State<Chats> {
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,28 +20,60 @@ class _ChatsState extends State<Chats> {
         Container(
             height: 35,
             width: double.infinity,
-            color: Colors.amber,
+            //color: Colors.amber,
             child: Row(
               children: [
                 SizedBox(width: 20,),
-                Icon(Icons.archive_outlined),
-                SizedBox(width: 10,),
+                Icon(Icons.archive_outlined,color: Colors.teal,size: 20,),
+                SizedBox(width: 20,),
                  InkWell(onTap: () {
                    print('Archived Page');
                  },
-                  child: Text('Archived'))
+                  child: Text('Archived',style: TextStyle(fontSize: 15),))
                 ],
               
             )
           ),
         SizedBox(height: 4,),
       
-        
+        ListTile(
+          onTap: (){},
+          leading: CircleAvatar(
+            radius: 25,
+           backgroundImage: AssetImage('Image/FB.jpg'),
+            ),
+          title: Text('Shakil'),
+          subtitle: Text('Bhai flutter pera lagche'),
+          trailing: Text('6:33 PM') ,
+         ),
+
+         ListTile(
+          onTap: (){},
+          leading: CircleAvatar(
+            radius: 25,
+           backgroundImage: AssetImage('Image/Picture01.jpg'),
+            ),
+          title: Text('Navil Kazi'),
+          subtitle: Text('Shakil phone dhoro na kno?'),
+          trailing: Text('3:54 PM') ,
+         ),
+
+        ListTile(
+          onTap: (){},
+          leading: CircleAvatar(
+            radius: 25,
+           backgroundImage: AssetImage('Image/Bhai.jpg'),
+            ),
+          title: Text('Almus Fuad'),
+          subtitle: Text('Tr kaz kmn chole?'),
+          trailing: Text('11:27 PM') ,
+         ),
+
        SizedBox(height: 6,),
        Container(
             height: 35,
             width: double.infinity,
-            color: Colors.amber,
+            //color: Colors.amber,
             child: Row(mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(width: 20,),
@@ -57,4 +90,5 @@ class _ChatsState extends State<Chats> {
       ),
     );
   }
+  
 }
